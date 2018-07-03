60feedf2b27f9fc97626d11cfe8b79f86ff6d73a
ImerAl <40179626+ImerAl@users.noreply.github.com>
2018-07-02 19:25:00 -0600

    Cambios

--- WindowsFormsApplication1/ActualizarDocente.Designer.cs
+++ WindowsFormsApplication1/ActualizarDocente.Designer.cs
@@ -303,6 +303,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
             this.ClientSize = new System.Drawing.Size(782, 430);
             this.Controls.Add(this.txtDireccion);
             this.Controls.Add(this.label5);
